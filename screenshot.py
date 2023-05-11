55170c067b4803966f4f1fb521e81680a742a6c9
Arman Arisman <arman.wu@gmail.com>
2023-05-11 11:28:30 +0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,5 @@
 import pyautogui
+import tkinter as tk
 
 def take_sc(output_path):
     try:
@@ -8,5 +9,13 @@ def take_sc(output_path):
     except Exception as e:
         print("Sorry! Failed to take screenshot:", str(e))
 
-output_file = "C:\screenshot.png"
-take_sc(output_file)
+output_file = "D:\layar.png"
+
+root = tk.Tk()
+root.title("Tangkap Layar")
+root.geometry("300x100")
+
+screenshot_button = tk.Button(root, text="Tangkap Layar!", command=take_sc(output_file))
+screenshot_button.pack(pady=30)
+
+root.mainloop()
