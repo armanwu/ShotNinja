38829316bde64ccc4fffa932b004db682417b15e
Arman Arisman <arman.wu@gmail.com>
2023-05-11 15:55:52 +0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,21 +1,30 @@
 import pyautogui
 import tkinter as tk
+import tkinter.messagebox as messagebox
+from datetime import datetime
+import os
 
-def take_ss(output_path):
+def take_ss():
     try:
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        output_dir = os.path.join(os.path.expanduser("~"), "Pictures")
+        output_file = os.path.join(output_dir, f"ss_{timestamp}.png")
         screenshot = pyautogui.screenshot()
-        screenshot.save(output_path)
-        print("Tangkapan layar berhasil disimpan di", output_path)
+        screenshot.save(output_file)
+        messagebox.showinfo("Done!", f"Screenshot saved successfully in {output_file}")
     except Exception as e:
-        print("Maaf! Gagal melakukan tangkapan layar:", str(e))
-
-output_file = "D:\layar.png"
+        messagebox.showerror("Error!", "Failed to take screenshot:\n" + str(e))
 
 root = tk.Tk()
-root.title("Tangkap Layar")
+root.title("Screenshot")
 root.geometry("300x100")
+root.resizable(False, False)
 
-ss_button = tk.Button(root, text="Tangkap Layar!", command=take_ss(output_file))
+def button_click():
+    take_ss()
+
+ss_button = tk.Button(root, text="Take Screenshot!", command=button_click)
 ss_button.pack(pady=30)
 
 root.mainloop()
